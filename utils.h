06a876f951628dfc9428453d54aec76aa090402e
antonin <antonin.autret@efrei.net>
2025-10-21 16:49:08 +0200

    ""

--- utils.h
+++ utils.h
@@ -17,11 +17,12 @@ typedef struct s_list t_list;
 typedef struct adjlist{
     t_list* list;
     int list_number;
-};
+}t_adjlist;
 
 t_cell* createCell(int vertex, float weight);
 t_list CreateEmptyList();
 void addcell(t_list* l,t_cell* c);
 void display_list(t_list l,int num);
+void display_adjlist(t_adjlist l);
 
 #endif
