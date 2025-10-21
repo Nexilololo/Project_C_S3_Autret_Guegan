cb38130ffdad0187228978b4b3e6cdab71b3372e
Charles Guégan <nexilog1706@gmail.com>
2025-10-21 15:12:19 +0200

    utils struct

--- utils.h
+++ utils.h
@@ -1,5 +1,18 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
+struct s_cell {
+    int vertex;
+    float weight;
+    struct s_cell * next;
+};
+typedef struct s_cell t_cell;
+
+struct s_list {
+    t_cell * head;
+};
+typedef struct s_list t_list;
+
+
 
 #endif
