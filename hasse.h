74bd9913868c3733a424f493f1dc1ce9f21dc238
CharlesG <nexilog1706@gmail.com>
2025-11-23 20:40:28 +0100

    Last Prototypes and Data Structures of Part 2

--- hasse.h
+++ hasse.h
@@ -1,7 +1,21 @@
 #ifndef __HASSE_H__
 #define __HASSE_H__
 
+typedef struct s_class_link {
+    int start_class;
+    int end_class;
+} t_class_link;
+
+
+typedef struct s_link_array {
+    t_class_link *links;
+    int count;
+} t_link_array;
+
 void removeTransitiveLinks(t_link_array *p_link_array);
+
+t_link_array getHasseLinks(t_adjlist graph, t_partition partition);
+void createHasseMermaid(t_partition partition, t_link_array links, char *filename);
 
 /**
  * @brief Creates a link array from the given partition and graph.
